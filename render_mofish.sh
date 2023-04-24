6872cb6e16e67264e0d6272d441042150fd5f022
abcdlsj <lisongjianshuai@gmail.com>
2023-04-24 11:21:20 +0800

    update render_mofish.sh

--- render_mofish.sh
+++ render_mofish.sh
@@ -1,5 +1,6 @@
-jq -r '.douban | map("- \(.title) \(.url)") | join("\n") | "douban:\n\(.)"' mofish.json
-echo
-jq -r '.hackernews | map("- \(.title) \(.url)") | join("\n") | "hackernews:\n\(.)"' mofish.json
-echo 
-jq -r '.hupu | map("- \(.title) \(.url)") | join("\n") | "hupu:\n\(.)"' mofish.json
+douban=$(jq -r '.douban | map("- \(.title) \u001b[34m\(.url)\u001b[0m") | join("\n") | "douban:\n\(.)"' mofish.json)
+hackernews=$(jq -r '.hackernews | map("- \(.title) \u001b[34m\(.url)\u001b[0m") | join("\n") | "hackernews:\n\(.)"' mofish.json)
+hupu=$(jq -r '.hupu | map("- \(.title) \u001b[34m\(.url)\u001b[0m") | join("\n") | "hupu:\n\(.)"' mofish.json)
+echo "$douban\n"
+echo "$hackernews\n"
+echo "$hupu"
